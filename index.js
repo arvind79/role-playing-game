e00e5a8eb41b9abe7ef3e969b44bf5b0b7caa519
arvind79 <81362519+arvind79@users.noreply.github.com>
2023-03-25 13:38:36 +0530

    divided javascript code in different files

--- index.js
+++ index.js
@@ -1,56 +1,11 @@
-const hero = {
-    elementId: "hero", 
-    name: "Wizard",
-    avatar: "images/wizard.png",
-    health: 60,
-    diceRoll: [3, 1, 4],
-    diceCount: 3
+import characterData from './data.js'
+import Character from '/Character.js'
+
+const wizard = new Character(characterData.hero) 
+const orc = new Character(characterData.orc)
+
+function render() {
+    document.getElementById('hero').innerHTML = wizard.getCharacterHtml()
+    document.getElementById('monster').innerHTML = orc.getCharacterHtml()
 }
-
-const monster = {
-    elementId: "monster",
-    name: "Orc",
-    avatar: "images/orc.png",
-    health: 10,
-    diceRoll: [2],
-    diceCount: 1
-}
-
-function getDiceRollArray(diceCount) {
-    return new Array(diceCount).fill(0).map(function() {
-        return Math.floor(Math.random() * 6) + 1
-    })
-}
-
-function Character(data) {
-
-    Object.assign(this, data)
-
-    this.getDiceHtml = function(diceCount) {
-        return getDiceRollArray(diceCount).map(function(num) {
-            return `<div class="dice">${num}</div>`
-        }).join('')
-    }
-
-    this.getCharacterHtml = function() {
-        const {elementId, name, avatar, health, diceCount} = this
-        const diceHtml = this.getDiceHtml(diceCount)
-        
-        return `
-            <div class="character-card">
-                <h4 class="name"> ${name} </h4>
-                <img class="avatar" src="${avatar}"/>
-                <p class="health">health: <b> ${health} </b></p>
-                <div class="dice-container">
-                    ${diceHtml}
-                </div>
-            </div> 
-        `
-    }
-}
-
-const wizard = new Character(hero) 
-const orc = new Character(monster)
-
-document.getElementById('hero').innerHTML = wizard.getCharacterHtml()
-document.getElementById('monster').innerHTML = orc.getCharacterHtml()
+render()
